859265f2fac38ba83f4d023bc08a9a717b9aa2d6
astrut12 <alon.strutsovsky@mail.huji.ac.il>
2013-09-15 23:00:04 +0300

    Sorry, added missing files

---
+++ ClimbXpert/src/com/example/climbxpert/CameraView.java
@@ -0,0 +1,51 @@
+package com.example.climbxpert;
+
+import java.io.IOException;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceHolder.Callback;
+import android.view.SurfaceView;
+
+public class CameraView extends SurfaceView implements Callback {
+	private SurfaceHolder mHolder;
+    private Camera mCamera;
+    private Activity mActivity;
+
+	public CameraView(Context context, Camera camera) {
+		super(context);
+        mCamera = camera;
+        mActivity = (Activity) context;
+
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+        // The Surface has been created, now tell the camera where to draw the preview.
+        try {
+            mCamera.setPreviewDisplay(holder);
+            mCamera.startPreview();
+        } catch (IOException e) {
+            LoggerTools.LogToast(mActivity, "Error setting camera preview: " + e.getMessage());
+        }
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+       // empty. Releasing the Camera preview in the calling Activity.		
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+		// empty. Suits only full horizontal view.
+	}
+
+}
---
+++ ClimbXpert/src/com/example/climbxpert/CameraViewActivity.java
@@ -0,0 +1,64 @@
+package com.example.climbxpert;
+
+import android.hardware.Camera;
+import android.media.ExifInterface;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.FrameLayout;
+
+public class CameraViewActivity extends Activity {
+
+    private Camera mCamera;
+    private CameraView mPreview;
+    
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_camera_view);
+		
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+        if (mCamera == null) {
+        	LoggerTools.LogToast(this, "could not initiate camera.");
+        	finishActivity(RESULT_CANCELED);
+        }
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraView(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_view);
+        preview.addView(mPreview);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.camera_view, menu);
+		return true;
+	}
+	
+	public static Camera getCameraInstance(){
+	    Camera c = null;
+	    try {
+	        c = Camera.open(); // attempt to get a Camera instance
+	    }
+	    catch (Exception e){
+	        // Camera is not available (in use or does not exist)
+	    }
+	    return c; // returns null if camera is unavailable
+	}
+	
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseCamera();              // release the camera immediately on pause event
+    }
+
+    private void releaseCamera(){
+        if (mCamera != null){
+            mCamera.release();        // release the camera for other applications
+            mCamera = null;
+        }
+    }
+
+}
