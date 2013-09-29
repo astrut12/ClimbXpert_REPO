19f0439e3017b1cbd8278ac2b938613673f048e4
Itai Lahat <itai.lahat.il@gmail.com>
2013-09-29 23:55:52 +0300

    t

--- ClimbXpert/src/com/example/climbxpert/CameraViewActivity.java
+++ ClimbXpert/src/com/example/climbxpert/CameraViewActivity.java
@@ -262,6 +262,7 @@ public class CameraViewActivity extends Activity
 	private void loadRoute(ClimbRoute route)
 	{
 		LoggerTools.LogToast(this, "rout: " + route.rid + " image: " + route.getImageId(this));
+		
 		//TODO fix overlapping in clicks
 		ImageButton imgButton = new ImageButton(this);
 		imgButton.setImageResource(route.getImageId(this)); 
